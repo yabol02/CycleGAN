4f469f3ebaeba95845bd231e3ba7aa2f6f8d3cf6
yabol02 <yagobf.ybf@gmail.com>
2024-05-30 19:42:12 +0200

    Cambios

--- cycleGAN_model.py
+++ cycleGAN_model.py
@@ -3,15 +3,10 @@ from numpy import load, zeros, ones, asarray
 from numpy.random import randint
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.initializers import RandomNormal
-from tensorflow.keras.models import Model, Input, Conv2D, Conv2DTranspose, LeakyReLU, Activation, Concatenate
-# from tensorflow.keras.layers import Input
-# from tensorflow.keras.layers import Conv2D
-# from tensorflow.keras.layers import Conv2DTranspose
-# from tensorflow.keras.layers import LeakyReLU
-# from tensorflow.keras.layers import Activation
-# from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, LeakyReLU, Activation, Concatenate
 import tensorflow_addons as tfa
-from matplotlib import pyplot
+import  matplotlib.pyplot as plt
 
 # Discriminator Model (PatchGAN 70x70 like in the original paper)
 def define_discriminator(image_shape):
@@ -77,10 +72,9 @@ def define_generator(image_shape, n_resnet=9):
 
 # Definition of a composite model for updating generators by adversarial and cycle loss. It will be used to train each generator separately. 
 def define_composite_model(g_model_1, d_model, g_model_2, image_shape):
-	# Make the generator of interest trainable as we will be updating these weights by keeping other models constant.
-    # Remember that we use this same function to train both generators, one generator at a time. 
+	# Make the generator of interest trainable as we will be updating these weights by keeping other models constant. This function is used to train both generators, one at a time. 
 	g_model_1.trainable = True
-	# mark discriminator and second generator as non-trainable
+	# Mark discriminator and second generator as non-trainable
 	d_model.trainable = False
 	g_model_2.trainable = False
     
@@ -103,11 +97,11 @@ def define_composite_model(g_model_1, d_model, g_model_2, image_shape):
     # Definition of the optimizer
 	opt = Adam(lr=0.0002, beta_1=0.5)
 	# Compilation of the model with weighting of least squares loss and L1 loss
-	model.compile(loss=['mse', 'mae', 'mae', 'mae'], loss_weights=[1, 5, 10, 10], optimizer=opt)
+	model.compile(loss=['mse', 'mse', 'mse', 'mse'], loss_weights=[1, 5, 10, 10], optimizer=opt)
 
 	return model
 
-# load and prepare training images
+# Load and prepare training images
 def load_real_samples(filename):
 	# Load of the dataset
 	data = load(filename)
@@ -118,137 +112,135 @@ def load_real_samples(filename):
 	X2 = (X2 - 127.5) / 127.5
 	return [X1, X2]
 
-# select a batch of random samples, returns images and target
-#Remember that for real images the label (y) is 1. 
+# Selection of a batch of random samples. Returns images and target (real images' label is 1) 
 def generate_real_samples(dataset, n_samples, patch_shape):
-	# choose random instances
+	# Choosing random instances
 	ix = randint(0, dataset.shape[0], n_samples)
-	# retrieve selected images
+	# Retrieving selected images
 	X = dataset[ix]
-	# generate 'real' class labels (1)
+	# Generating 'real' class labels (1)
 	y = ones((n_samples, patch_shape, patch_shape, 1))
 	return X, y
 
-# generate a batch of images, returns images and targets
-#Remember that for fake images the label (y) is 0. 
+# Generation of a batch of images. Returns images and targets (fake images' label is 0) 
 def generate_fake_samples(g_model, dataset, patch_shape):
-	# generate fake images
+	# Generating fake images
 	X = g_model.predict(dataset)
-	# create 'fake' class labels (0)
+	# Creating 'fake' class labels (0)
 	y = zeros((len(X), patch_shape, patch_shape, 1))
 	return X, y
 
-# periodically save the generator models to file
+# Save the generator models to file
 def save_models(step, g_model_AtoB, g_model_BtoA):
-	# save the first generator model
-	filename1 = 'g_model_AtoB_%06d.h5' % (step+1)
+	# Saving the first generator model
+	filename1 = f'g_model_AtoB_{step+1:08d}.h5'
 	g_model_AtoB.save(filename1)
-	# save the second generator model
-	filename2 = 'g_model_BtoA_%06d.h5' % (step+1)
+	# Saving the second generator model
+	filename2 = f'g_model_BtoA_{step+1:08d}.h5'
 	g_model_BtoA.save(filename2)
-	print('>Saved: %s and %s' % (filename1, filename2))
+	print(f'>Saved: {filename1} and {filename2}')
 
-# periodically generate images using the save model and plot input and output images
+# Generation of images using the save model and plot input and output images
 def summarize_performance(step, g_model, trainX, name, n_samples=5):
-	# select a sample of input images
+	# Selecting a sample of input images
 	X_in, _ = generate_real_samples(trainX, n_samples, 0)
-	# generate translated images
+	# Generating translated images
 	X_out, _ = generate_fake_samples(g_model, X_in, 0)
 	# scale all pixels from [-1,1] to [0,1]
 	X_in = (X_in + 1) / 2.0
 	X_out = (X_out + 1) / 2.0
-	# plot real images
+	# Plotting real images
 	for i in range(n_samples):
-		pyplot.subplot(2, n_samples, 1 + i)
-		pyplot.axis('off')
-		pyplot.imshow(X_in[i])
-	# plot translated image
+		plt.subplot(2, n_samples, 1 + i)
+		plt.axis('off')
+		plt.imshow(X_in[i])
+	# Plotting translated image
 	for i in range(n_samples):
-		pyplot.subplot(2, n_samples, 1 + n_samples + i)
-		pyplot.axis('off')
-		pyplot.imshow(X_out[i])
-	# save plot to file
-	filename1 = '%s_generated_plot_%06d.png' % (name, (step+1))
-	pyplot.savefig(filename1)
-	pyplot.close()
+		plt.subplot(2, n_samples, 1 + n_samples + i)
+		plt.axis('off')
+		plt.imshow(X_out[i])
+	# Saving plot to file
+	filename1 = f'{name}_generated_plot_{step+1:08d}.png'
+	plt.savefig(filename1)
+	plt.close()
 
-# update image pool for fake images to reduce model oscillation
-# update discriminators using a history of generated images 
-#rather than the ones produced by the latest generators.
-#Original paper recommended keeping an image buffer that stores 
-#the 50 previously created images.
-
+# Update the image pool for fake images to reduce model oscillation and the discriminators using a history of generated images rather than the ones produced by the latest generators.
+# Original paper recommended keeping an image buffer that stores the 50 previously created images.
 def update_image_pool(pool, images, max_size=50):
 	selected = list()
 	for image in images:
 		if len(pool) < max_size:
-			# stock the pool
+			# Stock the pool
 			pool.append(image)
 			selected.append(image)
 		elif random() < 0.5:
-			# use image, but don't add it to the pool
+			# Using the image, but don't adding it to the pool
 			selected.append(image)
 		else:
-			# replace an existing image and use replaced image
+			# Replacing an existing image and using the replaced image
 			ix = randint(0, len(pool))
 			selected.append(pool[ix])
 			pool[ix] = image
 	return asarray(selected)
 
-# train cyclegan models
-def train(d_model_A, d_model_B, g_model_AtoB, g_model_BtoA, c_model_AtoB, c_model_BtoA, dataset, epochs=1):
-	# define properties of the training run
-	n_epochs, n_batch, = epochs, 1  #batch size fixed to 1 as suggested in the paper
-	# determine the output square shape of the discriminator
+# Training of both CycleGAN models
+def train(d_model_A, d_model_B, g_model_AtoB, g_model_BtoA, c_model_AtoB, c_model_BtoA, dataset, losses, epochs=1):
+	# Defining properties of the training run
+	n_epochs, n_batch, = epochs, 1  # Batch size  is fixed to 1 as suggested in the paper
+	# Determining the output square shape of the discriminator
 	n_patch = d_model_A.output_shape[1]
-	# unpack dataset
+	# Unpacking the dataset
 	trainA, trainB = dataset
-	# prepare image pool for fake images
+	# Preparing the image pool for fake images
 	poolA, poolB = list(), list()
-	# calculate the number of batches per training epoch
-	bat_per_epo = int(len(trainA) / n_batch)
-	# calculate the number of training iterations
-	n_steps = bat_per_epo * n_epochs
+	# Calculating the number of batches per training epoch
+	batch_per_epoch = int(len(trainA) / n_batch)
+	# Calculating the number of training iterations
+	n_steps = batch_per_epoch * n_epochs
     
-	# manually enumerate epochs
+	# Enumerating the epochs manually
 	for i in range(n_steps):
-		# select a batch of real samples from each domain (A and B)
+		# Selecting a batch of real samples from each domain (A and B)
 		X_realA, y_realA = generate_real_samples(trainA, n_batch, n_patch)
 		X_realB, y_realB = generate_real_samples(trainB, n_batch, n_patch)
-		# generate a batch of fake samples using both B to A and A to B generators.
+		# Generating a batch of fake samples using both B to A and A to B generators
 		X_fakeA, y_fakeA = generate_fake_samples(g_model_BtoA, X_realB, n_patch)
 		X_fakeB, y_fakeB = generate_fake_samples(g_model_AtoB, X_realA, n_patch)
-		# update fake images in the pool. Remember that the paper suggstes a buffer of 50 images
+		# Updating fake images in the pool (the paper suggests a buffer of 50 images)
 		X_fakeA = update_image_pool(poolA, X_fakeA)
 		X_fakeB = update_image_pool(poolB, X_fakeB)
         
-		# update generator B->A via the composite model
+		# Updating generator B → A via the composite model
 		g_loss2, _, _, _, _  = c_model_BtoA.train_on_batch([X_realB, X_realA], [y_realA, X_realA, X_realB, X_realA])
-		# update discriminator for A -> [real/fake]
+		# Updating the discriminator for A → [real/fake]
 		dA_loss1 = d_model_A.train_on_batch(X_realA, y_realA)
 		dA_loss2 = d_model_A.train_on_batch(X_fakeA, y_fakeA)
 		
-        # update generator A->B via the composite model
+        # Updating the generator A → B via the composite model
 		g_loss1, _, _, _, _ = c_model_AtoB.train_on_batch([X_realA, X_realB], [y_realB, X_realB, X_realA, X_realB])
-		# update discriminator for B -> [real/fake]
+		# Updating the discriminator for B → [real/fake]
 		dB_loss1 = d_model_B.train_on_batch(X_realB, y_realB)
 		dB_loss2 = d_model_B.train_on_batch(X_fakeB, y_fakeB)
 		
-        # summarize performance
-        #Since our batch size =1, the number of iterations would be same as the size of our dataset.
-        #In one epoch you'd have iterations equal to the number of images.
-        #If you have 100 images then 1 epoch would be 100 iterations
-		print('Iteration>%d, dA[%.3f,%.3f] dB[%.3f,%.3f] g[%.3f,%.3f]' % (i+1, dA_loss1,dA_loss2, dB_loss1,dB_loss2, g_loss1,g_loss2))
-		# evaluate the model performance periodically
-        #If batch size (total images)=100, performance will be summarized after every 75th iteration.
-		if (i+1) % (bat_per_epo * 1) == 0:
-			# plot A->B translation
+		# Adding all losses to the dict with them
+		losses['dA1'].append(dA_loss1)
+		losses['dA2'].append(dA_loss2)
+		losses['dB1'].append(dB_loss1)
+		losses['dB2'].append(dB_loss2)
+		losses['g1'].append(g_loss1)
+		losses['g2'].append(g_loss2)
+
+        # Performance summary
+        # Since the batch size =1, the number of iterations should be the same as the size of the dataset. For example, having 100 images, an epoch will be 100 iterations.
+		print(f'Iteration>{i+1}, dA[{dA_loss1:.3f},{dA_loss2:.3f}] dB[{dB_loss1:.3f},{dB_loss2:.3f}] g[{g_loss1:.3f},{g_loss2:.3f}]')
+		# Evaluating the model performance periodically. For example, if batch size =100, performance will be summarized after every 75th iteration.
+		if (i+1) % (batch_per_epoch * 1) == 0:
+			# Plotting  A → B translation
 			summarize_performance(i, g_model_AtoB, trainA, 'AtoB')
-			# plot B->A translation
+			# Plotting B → A translation
 			summarize_performance(i, g_model_BtoA, trainB, 'BtoA')
-		if (i+1) % (bat_per_epo * 5) == 0:
-			# save the models
-            # #If batch size (total images)=100, model will be saved after 
-            #every 75th iteration x 5 = 375 iterations.
+		if (i+1) % (batch_per_epoch * 5) == 0:
+			# Save the models. For example, if  the batch size =100, model will be saved after every 75th iteration x 5 = 375 iterations.
 			save_models(i, g_model_AtoB, g_model_BtoA)
+	return losses
 
